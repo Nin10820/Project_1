2bbab15958abf09df2b55a1bbbe855f7737e9cf4
Nin10820 <Minhnguyen1082000@gmail.com>
2020-08-22 15:15:00 +0700

    update nav 1

---
+++ src/views/Shop.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="shop mt-5">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo reiciendis quam quod deleniti velit odio id! Dignissimos aperiam quos omnis maiores neque et quod repellendus minus, quidem nulla quaerat?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, temporibus dolore? Repellat placeat illum facilis expedita inventore, explicabo rem in, exercitationem dolore laudantium, reprehenderit iste obcaecati deleniti cumque. Cupiditate, porro</h1>
+    </div>
+</template>
